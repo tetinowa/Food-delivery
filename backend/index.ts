9ad0eff8a6ddf4ad5c0d03114aee82c88e23efca
Rafina Rosenfeld <tetinowa@Rafinas-MacBook-Air.local>
2025-12-25 11:29:03 +0800

    aa

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import { connectToDatabase } from "./database/index.ts";
-import { foodRouter } from "./routes/index.ts";
+import { connectToDatabase } from "./src/database/index.ts";
+import { foodRouter } from "./src/food.router.ts";
 
 await connectToDatabase();
 
